2db75f55210e1316d84a97c0353a01211da0baf8
Ben Lei <benjamin.g.lei@gmail.com>
2016-02-24 04:47:57 -0800

    now supports retry button, fixed formatting

--- SieveOfEratosthenes.rb
+++ SieveOfEratosthenes.rb
@@ -10,7 +10,7 @@ get '/' do
 end
 
 # Displays table upon entering input
-post '/' do
+post '/1' do
   @input = params[:input]
   #slim :input
   if !valid_input(@input) || @input.to_s.empty?
@@ -19,8 +19,13 @@ post '/' do
     @inputNumber = Integer(@input)
     @size = Math.sqrt(@inputNumber).ceil
     @table = Table.new(@size,@size,@inputNumber)
+    #@table.sieve()
     slim :tableview
   end
+end
+
+post "/" do
+  slim:index
 end
 
 # A regex function for checking valid input
--- Table.rb
+++ Table.rb
@@ -1,35 +1,54 @@
 class Table
-  attr_accessor :space
+  attr_accessor :space, :numcols, :numrows
   def initialize(cols, rows, inputNumber)
-    @iterator = 1
+    iterator = 1
+    @numcols = cols
+    @numrows = rows
     @space = []
     y = 0
     x = 0
     rows.times do
       row = []
       cols.times do
-        if @iterator > inputNumber
+        if iterator > inputNumber
           break
         end
-        square = Square.new({:color => 'grey'})
+        square = Square.new("grey")
         square.y = y
         square.x = x
-        square.value = @iterator
+        square.value = iterator
+        square.truth = true
         row.push square
         x += 1
-        @iterator += 1
+        iterator += 1
       end
       y += 1
       x = 0
       @space.push row
     end
+    #@space[0][0].color = "red"
+  end
+  # The actual Sieve algorithm (WIP)
+  def sieve()
+    @space[0][0].color = "red"
+    counter = 0
+    @space.each do |p|
+      next unless p
+      break if p*p > inputNumber
+      counter += 1
+      (p*p).step(inputNumber,p) { |m| @space[m/@numcols][m % @numrows] = nil}
+    end
   end
 end
 
 class Square
-  attr_reader :color, :x, :y, :value
-  def initialize(args)
-    @color = args['color'] || 'grey'
+  attr_reader :color, :x, :y, :value, :truth
+  def initialize(color)
+    @color = color
+  end
+
+  def color=(color)
+    @color = color
   end
 
   def x=(x)
@@ -42,5 +61,9 @@ class Square
 
   def value=(value)
     @value = value
+  end
+
+  def truth=(truth)
+    @truth = truth
   end
 end
