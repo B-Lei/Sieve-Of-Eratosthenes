afa2323226abb01d45fdd9b208abd8f70c051907
Ben Lei <benjamin.g.lei@gmail.com>
2016-02-23 20:01:00 -0800

    app now handles invalid input

--- SieveOfEratosthenes.rb
+++ SieveOfEratosthenes.rb
@@ -12,9 +12,18 @@ end
 # Displays table upon entering input
 post '/' do
   @input = params[:input]
-  slim :input
-  @inputNumber = Integer(@input)
-  @size = Math.sqrt(@inputNumber).ceil
-  @table = Table.new(@size,@size,@inputNumber)
-  slim :tableview
+  #slim :input
+  if !valid_input(@input) || @input.to_s.empty?
+    slim :tryagain
+  else
+    @inputNumber = Integer(@input)
+    @size = Math.sqrt(@inputNumber).ceil
+    @table = Table.new(@size,@size,@inputNumber)
+    slim :tableview
+  end
+end
+
+# A regex function for checking valid input
+def valid_input(input)
+  /^[1-9]\d*$/ === input
 end
